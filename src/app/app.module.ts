8cf7fcb62bb97ec2cdcbc7448d9b12f228d828a1
pedrogomes0008 <pedrogomes0008@gmail.com>
2025-02-16 12:58:03 +0000

    Refactor application structure by introducing SharedModule and TasksModule for better modularity

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
-import { TasksComponent } from "./tasks/tasks.component";
-import { CardComponent } from "./shared/card/card.component";
-import { TaskComponent } from "./tasks/task/task.component";
-import { NewTaskComponent } from "./tasks/task/new-task/new-task.component";
+import { SharedModule } from "./shared/shared.module";
+import { TasksModule } from "./tasks/tasks.module";
 
 @NgModule({
-    declarations: [AppComponent, HeaderComponent, UserComponent, CardComponent, TasksComponent, TaskComponent, NewTaskComponent], // For non standalone components
+    declarations: [
+        AppComponent, 
+        HeaderComponent, 
+        UserComponent
+    ],
     bootstrap: [AppComponent],
-    imports: [BrowserModule, FormsModule] // For standalone components
+    imports: [BrowserModule, SharedModule, TasksModule]
 })
 export class AppModule {}
